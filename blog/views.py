3185c7c5e17e2f2bf04e03e4b5c2ee08dbce3151
WhereMania <WhereMania@gmail.com>
2024-12-01 11:17:56 +0330

    added catgegory page  and  url

--- blog/views.py
+++ blog/views.py
@@ -13,8 +13,25 @@ def blog_single(request,pid):
     context = {'post':post}
     return render(request, "blog/blog-single.html", context)
 
+def blog_category(request,cat_name):
+    posts = Post.objects.filter(status=1)
+    posts = posts.filter(category__name=cat_name) # we cant use single category here becvasue it look at id's and if we want to check name we need use category__name or any other variable we need
+    context = {'posts':posts}
+    return render(request, "blog/blog-home.html",context)
+
+
+
+
+
+
+
+
+
+
+
 def test(request): #we can use pid to take only a single paramether out for example 1 post for display instead of taking everything out
    # posts = get_object_or_404(Post,id=pid,status=1) # Query to data base and get all we can also use get , filter or  get_object_or_404
     #posts = Post.objects.filter(status=1) # it means if post was published then we display it to screen other wise if status=0 it not gonna show those since they are not published
     #context = {'posts':posts} # use context and use a variable 'posts' and equal to our posts and then we use it inside the template for display
     return render(request,"test.html")
+
